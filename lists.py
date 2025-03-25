b6590b2134128e5a7354cd4232bce9793250d867
Tracey <145255568+Traceeey@users.noreply.github.com>
2025-03-25 21:36:16 +0300

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,16 @@
+my_list = []
+
+my_list = [10, 20, 30, 40, 50]
+print(my_list)
+
+my_list.insert(2, 15)
+print(my_list)
+
+my_list.extend([50, 60, 70])
+print(my_list)
+
+my_list.sort()
+print(my_list)
+
+index_of_value = my_list.index(30)
+print(index_of_value)
